27e47889f742a745d0ba19ad91b97e919ce1f0f5
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-05-12 10:09:13 -0300

    @php @foreach

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de usuários</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
     {{ $greeting }} <!-- Váriavel que veio da controller -->
+
+    @php #Blocos php
+
+        $name = "Tony";
+
+    @endphp 
+
+    @foreach($users as $user)
+        <div>
+            Nome: {{$user->name}} / Email: {{ $user->email }}
+        </div>
+    @endforeach
+
+    {{ $name }}
     {{ dd($users) }}
 </body>
 </html>
