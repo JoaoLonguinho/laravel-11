dfca463257a4c3d337ec98169b484ec86724d4e1
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-05-16 09:21:42 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
 
+    protected $with = ['profile']; # carrega automaticamente junto com a relação
     public function profile()
     {
         return $this->hasOne(Profile::class);
